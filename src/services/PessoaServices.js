5ff144dce29c171287bb99b2e1f39c3d56de9273
felipe <felipeadar108@gmail.com>
2025-01-09 14:39:50 -0300

    :sparkles: feat: criando método para buscar pessoas por escopo

--- src/services/PessoaServices.js
+++ src/services/PessoaServices.js
@@ -10,6 +10,11 @@ class PessoaServices extends Services {
     const listaMatriculas = await estudante.getAulasMatriculadas();
     return listaMatriculas;
   }
+
+  async pegaPessoasEscopoTodos() {
+    const listaPessoas = await super.pegaRegistrosPorEscopo('todos');
+    return listaPessoas;
+  }
 }
 
 module.exports = PessoaServices;
