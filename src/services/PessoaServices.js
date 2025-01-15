06ab08fbf24e18a71340e63e01a98d6c4b55f35c
felipe <felipeadar108@gmail.com>
2025-01-15 17:05:33 -0300

    :sparkles: feat: adicionando transaction

--- src/services/PessoaServices.js
+++ src/services/PessoaServices.js
@@ -1,3 +1,4 @@
+const dataSource = require('../database/models');
 const Services = require('./Services.js');
 
 class PessoaServices extends Services {
@@ -24,8 +25,10 @@ class PessoaServices extends Services {
   }
 
   async cancelaPessoaEMatriculas(estudanteId) {
-    await super.atualizaRegistro({ ativo: false }, { id: estudanteId });
-    await this.matriculaServices.atualizaRegistro({ status: 'cancelado' }, { estudante_id: estudanteId });
+    return dataSource.sequelize.transaction(async (transaction) => {
+      await super.atualizaRegistro({ ativo: false }, { id: estudanteId }, transaction);
+      await this.matriculaServices.atualizaRegistro({ status: 'cancelado' }, { estudante_id: estudanteId }, transaction);
+    });
   }
 }
 
--- src/services/Services.js
+++ src/services/Services.js
@@ -29,10 +29,12 @@ class Services {
     return dataSource[this.model].create(dadosDoRegistro);
   }
 
-  async atualizaRegistro(dadosAtualizados, where) {
-    const listadeRegistrosAtualizados = await dataSource[this.model].update(dadosAtualizados, {
-      where: { ...where }
-    });
+  async atualizaRegistro(dadosAtualizados, where, transaction = {}) {
+    const listadeRegistrosAtualizados = await dataSource[this.model]
+      .update(dadosAtualizados, {
+        where: { ...where },
+        transaction: transaction,
+      });
     if (listadeRegistrosAtualizados[0] === 0) {
       return false;
     }
